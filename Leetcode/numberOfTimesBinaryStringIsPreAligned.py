47c8fb1bb95441eea7109179f590ee1babae993f
Anmol-Nagpal <nagpaanm@sheridancollege.ca>
2022-02-19 12:12:29 -0500

    solved numberOfTimesBinaryStringIsPreAligned problem (leetcode)

--- Leetcode/numberOfTimesBinaryStringIsPreAligned.py
+++ Leetcode/numberOfTimesBinaryStringIsPreAligned.py
@@ -6,17 +6,12 @@ Created on Feb. 18, 2022
 
 class Solution:
     def numTimesAllBlue(self, flips: List[int]) -> int:
-        binaryArr = [0] * len(flips)
-        total = 0
+        total = 0;
         maxFound = 0;
-        for i in range(len(flips)):
-            binaryArr[flips[i] - 1] = 1
-            pa = True
-            #print(binaryArr)
-            if(flips[i] < maxFound or flips[i] == maxFound + 1):
-                #if "".join(str(item) for item in binaryArr[0:maxFound]) == "1" * maxFound: 
-                summed =[item for item in binaryArr[0:maxFound]]
-                if sum(summed) == maxFound:
-                    total += 1
-            maxFound = max(maxFound, flips[i])
+        found = 0;
+        for num in flips:
+            found += 1;
+            maxFound = max(maxFound, num)
+            if maxFound == found:
+                total += 1
         return total
