82765d9f39dbf552dd4aed5543aa8225c1a6aef8
Anmol-Nagpal <nagpaanm@sheridancollege.ca>
2022-02-20 19:07:55 -0500

    solved coureScheduleii problem (leetcode)

--- Leetcode/courseScheduleii.py
+++ Leetcode/courseScheduleii.py
@@ -6,13 +6,20 @@ Created on Feb. 20, 2022
 
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        result = list()
-        if(len(prerequisites) == 0):
-            return [item for item in range(numCourses)]
-        for lst in prerequisites:
-            if lst[0] not in result:
-                result.append(lst[0])
-            if lst[1] not in result:
-                result.append(lst[1])
-        result = sorted(result)
-        return result
+        indegree = [set() for _ in range(numCourses)]
+        outdegree = [[] for _ in range(numCourses)]
+        for p in prerequisites:
+            indegree[p[0]].add(p[1])
+            outdegree[p[1]].append(p[0])
+        ret = []
+        start = [i for i in range(numCourses) if not indegree[i]]
+        while start: # start contains courses without prerequisites
+            newStart = [] 
+            for i in start:
+                ret.append(i)
+                for j in outdegree[i]:
+                    indegree[j].remove(i)
+                    if not indegree[j]:
+                        newStart.append(j)
+            start = newStart # newStart contains new courses with no prerequisites
+        return ret if len(ret) == numCourses else [] # can finish if ret contains all courses 
