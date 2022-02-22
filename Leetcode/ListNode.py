e0f41d58d7fb84819b2c1d8a76276e1695e8f427
Anmol-Nagpal <nagpaanm@sheridancollege.ca>
2022-02-21 23:55:03 -0500

    solved linkedListCycle problem (leetcode)

---
+++ Leetcode/ListNode.py
@@ -0,0 +1,10 @@
+'''
+Created on Feb. 21, 2022
+
+@author: Anmol Nagpal
+'''
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
